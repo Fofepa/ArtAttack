eff324a706222c8ba9278633087d47b8c7bd2d45
mmlst <listimiriam@gmail.com>
2025-12-11 11:37:11 +0100

    Added not implemented class Give

---
+++ artattack/src/main/java/com/artattack/Give.java
@@ -0,0 +1,22 @@
+package com.artattack;
+
+public class Give implements Interaction {
+
+    //Attributes
+    private Item item;
+
+    //Constructor
+    public Give(){
+
+    }
+
+    //Primitive method
+    public void interact(){
+
+    }
+
+    //Getter
+    public Item item(){
+        return this.item;
+    }
+}

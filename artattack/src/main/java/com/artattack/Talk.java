b9d5ef4d1568bc7f1ffb4d886935eac7ff9a7132
mmlst <listimiriam@gmail.com>
2025-12-11 11:33:42 +0100

    Added not implemented class Talk

---
+++ artattack/src/main/java/com/artattack/Talk.java
@@ -0,0 +1,22 @@
+package com.artattack;
+
+public class Talk implements Interaction {
+
+    //Attribute
+    private String dialog;
+    
+    //Constructor
+    public Talk(){
+
+    }
+
+    //Primitive method
+    public void interact(){
+
+    }
+
+    //Getter
+    public String getDialog(){
+        return this.dialog;
+    }
+}

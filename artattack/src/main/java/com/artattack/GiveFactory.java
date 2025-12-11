4d8e04c63edb75fd1d8ca5c43815350373bf56d3
mmlst <listimiriam@gmail.com>
2025-12-11 11:45:58 +0100

    Added not implemented classes TalkFactory and GiveFactory

---
+++ artattack/src/main/java/com/artattack/GiveFactory.java
@@ -0,0 +1,8 @@
+package com.artattack;
+
+public class GiveFactory implements InteractionFactory {
+    //Primitive method
+    public Interaction createInteraction(){
+        
+    }
+}
---
+++ artattack/src/main/java/com/artattack/TalkFactory.java
@@ -0,0 +1,8 @@
+package com.artattack;
+
+public class TalkFactory implements InteractionFactory {
+    //Primitive method
+    public Interaction createInteraction(){
+        
+    }
+}

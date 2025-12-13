24e310dcca0c6a2ec0cdfa5e830212b8181f00e9
Davide11000 <davidelentini34@gmail.com>
2025-12-13 22:38:06 +0100

    Co-authored-by: StefanoCC <Stefanocccccccc@users.noreply.github.com>

--- artattack/src/main/java/com/artattack/Maps.java
+++ artattack/src/main/java/com/artattack/Maps.java
@@ -3,10 +3,10 @@ package com.artattack;
 import java.util.Map;
 
 public class Maps {
-    private Map<Coordinates, ? extends MapElement> dictionaire;
+    private Map<Coordinates, ? extends MapElement> dictionaire; // for now we leave it here
     private char[][] mapMatrix;
-    private int row;
-    private int column;
+    private int rows;
+    private int columns;
     
    /* public Maps(Map<Coordinates, ? extends MapElement> dictionaire, char[][] mapMatrix){
         this.dictionaire = dictionaire;
@@ -22,14 +22,25 @@ public class Maps {
     }
 
     public int getRow(){
-        return this.row;
+        return this.rows;
     }
 
-    public int getColumn(){
-        return this.column;
+    public int getcolumns(){
+        return this.columns;
     }
 
-    public boolean updateMovement(Coordinates curr_pos, Coordinates new_pos){
+    public void setCell(int row, int column, char character){
+        if(row >= 0 && row < rows && column >= 0 && column < columns)
+            this.mapMatrix[row][column] = character;
+    }
+
+    public char getCell(int row, int column){
+        if(row >= 0 && row < rows && column >= 0 && column < columns)
+            return this.mapMatrix[row][column];
+        else
+            return ' ';
+    }
+   /*  public boolean updateMovement(Coordinates curr_pos, Coordinates new_pos){ Should be inside the execute() of the MovementStrategy
 
         char symbol = this.mapMatrix[curr_pos.getX()][curr_pos.getY()];
 
@@ -39,5 +50,7 @@ public class Maps {
         }
         else
             return false;
-    }
+    } */
+
+    
 }

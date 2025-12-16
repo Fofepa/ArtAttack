186fbb62e2233848044a3ce488fd404eab8ef26a
Fofepa <jacoalf02@gmail.com>
2025-12-16 12:32:04 +0100

    Bug on windows possible fix

--- artattack/src/main/java/com/artattack/view/MapPanel.java
+++ artattack/src/main/java/com/artattack/view/MapPanel.java
@@ -78,24 +78,66 @@ class MapPanel extends JPanel {
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-    
-        Font font = new Font("Monospaced", Font.BOLD, 16);
-        g.setFont(font);
-        FontMetrics fm = g.getFontMetrics();
+
+        // --- 1. Definizione della dimensione base del font ---
+        // Usiamo una dimensione base per calcolare le dimensioni necessarie della mappa.
+        final int BASE_FONT_SIZE = 16;
+        Font baseFont = new Font("Monospaced", Font.BOLD, BASE_FONT_SIZE);
+        g.setFont(baseFont);
+        FontMetrics fmBase = g.getFontMetrics();
         
-        int charWidth = fm.charWidth('W');
-        int charHeight = fm.getHeight();
+        // Calcola le metriche di base per un font di dimensione 16
+        int baseCharWidth = fmBase.charWidth('W');
+        int baseCharHeight = fmBase.getHeight();
         
+        // Calcola le dimensioni totali richieste dalla mappa se fosse disegnata a dimensione base (16)
+        int mapRequiredWidth = this.movementStrategy.getMap().getColumns() * baseCharWidth;
+        int mapRequiredHeight = this.movementStrategy.getMap().getRows() * baseCharHeight;
+        
+        // --- 2. Calcolo del Fattore di Scala ---
+        
+        // Larghezza e altezza disponibili nel pannello
+        int panelWidth = getWidth();
+        int panelHeight = getHeight();
+        
+        // Calcola il fattore di scala per larghezza e altezza
+        double scaleX = (double) panelWidth / mapRequiredWidth;
+        double scaleY = (double) panelHeight / mapRequiredHeight;
+        
+        // Usa il fattore di scala MINIMO per garantire che l'intera mappa sia visibile (si adatti)
+        double scaleFactor = Math.min(scaleX, scaleY);
+
+        // Se la mappa è troppo piccola, non ingrandirla (opzionale, per evitare pixel troppo grandi)
+        // scaleFactor = Math.min(1.0, scaleFactor); 
+        
+        // --- 3. Applicazione del Fattore di Scala ---
+        
+        // Calcola la nuova dimensione del font e le metriche scalate
+        int scaledFontSize = (int) (BASE_FONT_SIZE * scaleFactor);
+        // Assicurati che la dimensione minima sia 1 per evitare errori
+        if (scaledFontSize < 1) scaledFontSize = 1; 
+
+        Font scaledFont = new Font("Monospaced", Font.BOLD, scaledFontSize);
+        g.setFont(scaledFont);
+        FontMetrics fmScaled = g.getFontMetrics();
+
+        int charWidth = fmScaled.charWidth('W');
+        int charHeight = fmScaled.getHeight();
+        
+        // Calcola le dimensioni finali della mappa scalata
         int mapLength = this.movementStrategy.getMap().getColumns() * charWidth;
         int mapHeight = this.movementStrategy.getMap().getRows() * charHeight;
         
-        int offsetX = (getWidth() - mapLength) / 2;
-        int offsetY = (getHeight() - mapHeight) / 2;
+        // Calcola gli offset per centrare la mappa
+        int offsetX = (panelWidth - mapLength) / 2;
+        int offsetY = (panelHeight - mapHeight) / 2;
+        
+        // --- 4. Disegno della Mappa (utilizzando le metriche scalate) ---
         
         for (int i = 0; i < this.movementStrategy.getMap().getRows(); i++) {
             for (int j = 0; j < this.movementStrategy.getMap().getColumns(); j++) {
                 char c = this.movementStrategy.getMap().getMapMatrix()[i][j];
-                
+
                 switch (c) {
                     case '#' -> g.setColor(Color.GRAY);
                     case '@' -> g.setColor(Color.GREEN);
@@ -104,18 +146,20 @@ class MapPanel extends JPanel {
                     case '.' -> g.setColor(new Color(50, 50, 50));
                     default -> g.setColor(Color.WHITE);
                 }
-                
+
                 g.drawString(String.valueOf(c), 
                     j * charWidth + offsetX, 
                     i * charHeight + offsetY + charHeight);
             }
         }
 
+        // --- 5. Disegno del Cursore (utilizzando le metriche scalate) ---
+
         int cursorX = movementStrategy.getCursor().getX();
         int cursorY = movementStrategy.getCursor().getY();
         int playerX = movementStrategy.getPlayer().getCoordinates().getX();
         int playerY = movementStrategy.getPlayer().getCoordinates().getY();
-        
+
         if (movementStrategy.getSelectedState() && showCursor && 
             (cursorX != playerX || cursorY != playerY)) {
             g.setColor(Color.CYAN);

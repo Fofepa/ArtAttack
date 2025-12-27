39a4da0891ea2492a6a8dab6e8597caf6112bb21
alex-grg <kurukiddo_@hotmail.com>
2025-12-27 17:09:04 +0100

    Modified moveTest class to accomodate new attack method from Move class

--- artattack/src/test/java/com/artattack/moveTest.java
+++ artattack/src/test/java/com/artattack/moveTest.java
@@ -1,7 +1,7 @@
 package com.artattack;
 
-import java.util.List;
 import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.After;
 import static org.junit.Assert.assertEquals;
@@ -11,16 +11,45 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class moveTest {
-
     private Move m1, m2, m3, m4;
+    private MoveBuilder1 mb1;
     private Player p1, p2;
     private Enemy e;
     private Maps maps;
 
     @Before
     public void setUp() throws Exception {
-        m1 = new Move("TestMove", "TestDescription", 1, List.of(new Coordinates(0, 1)));
-        m2 = new Move("TestMove", "TestDescription", 1, List.of(new Coordinates(1, 0)));
+        //Initializing MoveBuilder1
+        mb1 = new MoveBuilder1();
+        //Creating m1
+        mb1.setName("TestMove");
+        mb1.setDescription("TestDescription");
+        mb1.setPower(1);
+        mb1.setActionPoints(1);
+        mb1.setAttackArea(List.of(new Coordinates(0, 1)));
+        m1 = mb1.getResult();
+        //Creating m2
+        mb1.setName("TestMove");
+        mb1.setDescription("TestDescription");
+        mb1.setPower(1);
+        mb1.setActionPoints(1);
+        mb1.setAttackArea(List.of(new Coordinates(1, 0)));
+        m2 = mb1.getResult();
+        //Creating m3
+        mb1.setName("TestMove");
+        mb1.setDescription("TestDescription");
+        mb1.setPower(8);
+        mb1.setActionPoints(1);
+        mb1.setAttackArea(List.of(new Coordinates(0, 1)));
+        m3 = mb1.getResult();
+        //Creating m4
+        mb1.setName("TestMove");
+        mb1.setDescription("TestDescription");
+        mb1.setPower(2);
+        mb1.setActionPoints(1);
+        mb1.setAttackArea(List.of(new Coordinates(-1, 0)));
+        m4 = mb1.getResult();
+
         p1 = new MovieDirector(0, 'i', "TestPlayerOne", new Coordinates(0, 0),
             null, 0, null, 10, 10, 0, 10, 0, 0, 0, null,null,null);
         p2 = new Musician(0, 'i', "TestPlayerTwo", new Coordinates(1, 1),
@@ -28,8 +57,8 @@ public class moveTest {
         e = new Enemy(0, 'i', "TestEnemy", new Coordinates(0, 1), 10, 10, 0, null, 0, null,null,List.of(new Cure("cure", " ", 10)),10);
         maps = new Maps(p1, p2, null, List.of(e));
 
-        m3 = new Move("TestMove", "TestDescription", 8, List.of(new Coordinates(0, 1)));
-        m4 = new Move("TestMove", "TestDescription", 2, List.of(new Coordinates(-1, 0)));
+        /*m3 = new Move("TestMove", "TestDescription", 8, List.of(new Coordinates(0, 1)));
+        m4 = new Move("TestMove", "TestDescription", 2, List.of(new Coordinates(-1, 0)));*/
 
         assertNotNull(m1);
         assertNotNull(p1);
@@ -63,13 +92,20 @@ public class moveTest {
     public void testAttack() {
         //m1.attack(p1, maps);
         //assertEquals("Move.attack(MapElement attacker, Maps map) has failed", 9, e.getCurrHP()); //Player attacks Enemy
+        e.setActionPoints(m2.getActionPoints());
         m2.attack(e, maps);
         assertEquals("Move.attack(MapElement attacker, Maps map) has failed", 9, p2.getCurrHP()); //Enemy attacks Player*/
+        p1.setActionPoints(m3.getActionPoints());
         m3.attack(p1,maps);
+        p2.setActionPoints(m4.getActionPoints());
         m4.attack(p2,maps);
         assertEquals("Move.attack(MapElement attacker, Maps map) has failed", 0, e.getCurrHP());
         assertEquals("testAttack faild. p1 currXP not as expected.", 8, p1.getCurrXP());
         assertEquals("testAttack faild. p2 currXP not as expected.", 2, p2.getCurrXP());
         assertEquals("testAttack faild. p2 inventory not as expected.", 1, p2.getInventory().size());
+
+        //Testing insufficient ActionPoints
+        p1.setActionPoints(0);
+        assertEquals("Move.attack(MapElement attacker, Maps map) has failed", 0, m1.attack(p1, maps));
     }
 }

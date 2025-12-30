a4b2ba045024bd288f5e5197b6e1de23b306665f
Davide11000 <davidelentini34@gmail.com>
2025-12-30 11:14:56 +0100

    Fixed the costructor of Enemy in movementstrategyTest

--- artattack/src/test/java/com/artattack/movementstrategyTest.java
+++ artattack/src/test/java/com/artattack/movementstrategyTest.java
@@ -29,7 +29,7 @@ public class movementstrategyTest {
          )),
          List.of(
             new Enemy(0, 'E', "Goblin", new Coordinates(2, 3), 10, 10, 1, null, 10, null, List.of(new Coordinates(1, 1)), null,null, 0),
-            new Enemy(1, 'E', "Orco", new Coordinates(25, 25), 20, 20, 2, null, 15, null, List.of(new Coordinates(10, 10)), null,null ,0));
+            new Enemy(1, 'E', "Orco", new Coordinates(25, 25), 20, 20, 2, null, 15, null, List.of(new Coordinates(10, 10)), null,null ,0)));
         assertNotNull(map);
         move = new MovementStrategy(map, new Musician(0,'M',"Frank Zappa",new Coordinates(1,1)));
         assertNotNull(move);

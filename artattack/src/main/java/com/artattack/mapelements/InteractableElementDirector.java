1a391f9c1a995f3cfadab72480cd6e700d7ed3f0
mmlst <listimiriam@gmail.com>
2026-02-10 20:47:41 +0100

    Finished implementing InteractableElementDirector.

--- artattack/src/main/java/com/artattack/mapelements/InteractableElementDirector.java
+++ artattack/src/main/java/com/artattack/mapelements/InteractableElementDirector.java
@@ -2,9 +2,12 @@ package com.artattack.mapelements;
 
 import java.util.List;
 
+import com.artattack.interactions.CheckPoint;
 import com.artattack.interactions.Give;
+import com.artattack.interactions.SwitchMap;
 import com.artattack.interactions.Talk;
 import com.artattack.items.Item;
+import com.artattack.level.Coordinates;
 
 public class InteractableElementDirector {
     public void createChest(InteractableElementBuilder builder, List<Item> item){
@@ -16,15 +19,47 @@ public class InteractableElementDirector {
     }
 
     public void createCheckPoint(InteractableElementBuilder builder){
-        
+        builder.setMapSymbol('C');
+        builder.setName("Checkpoint");
+        builder.setInteractions(
+            List.of(new CheckPoint(List.of("Your progress has been saved")))
+        );
 
     }
-    /*public void createDoor(InteractableElementBuilder builder, int nextMap){
+    
+    public void createDoor(InteractableElementBuilder builder, int nextMap, List<Coordinates> nexCoordinates){
         builder.setMapSymbol('\u2339');
         builder.setName("Door");
         builder.setInteractions(
-            List.of(new SwitchMap(,))
+            List.of(new SwitchMap(nextMap, nexCoordinates))
         );
         
-    }*/
+    }
+
+    public void createDoor(InteractableElementBuilder builder, int nextMap, List<Coordinates> nexCoordinates, boolean isLevelFinish){
+        builder.setMapSymbol('\u2339');
+        builder.setName("Door");
+        builder.setInteractions(
+            List.of(new SwitchMap(nextMap, nexCoordinates, isLevelFinish))
+        );
+        
+    }
+
+    public void createDoor(InteractableElementBuilder builder, int nextMap, List<Coordinates> nexCoordinates, int key){
+        builder.setMapSymbol('\u2339');
+        builder.setName("Door");
+        builder.setInteractions(
+            List.of(new SwitchMap(key, nextMap, nexCoordinates))
+        );
+        
+    }
+
+     public void createDoor(InteractableElementBuilder builder, int nextMap, List<Coordinates> nexCoordinates, boolean isLevelFinish, int key){
+        builder.setMapSymbol('\u2339');
+        builder.setName("Door");
+        builder.setInteractions(
+            List.of(new SwitchMap(key, nextMap, nexCoordinates, isLevelFinish))
+        );
+        
+    }
 }
